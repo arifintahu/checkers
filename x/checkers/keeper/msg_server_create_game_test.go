91906a1dc00cc984a101b6a722415fe11073e600
Miftahul Arifin <miftahul97@gmail.com>
2022-09-25 08:52:17 +0700

    test: add msg server create game test

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,9 +1,14 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "github.com/arifintahu/checkers/testutil/keeper"
+	"github.com/arifintahu/checkers/x/checkers"
+	"github.com/arifintahu/checkers/x/checkers/keeper"
 	"github.com/arifintahu/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,6 +17,12 @@ const (
 	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
+
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
 
 func TestCreateGame(t *testing.T) {
 	msgServer, context := setupMsgServer(t)
@@ -22,6 +33,29 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "",
+		GameIndex: "1",
 	}, *createResponse)
+}
+
+func TestCreate1GameHasSaved(t *testing.T) {
+    msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+    msgSrvr.CreateGame(context, &types.MsgCreateGame{
+        Creator: alice,
+        Black:   bob,
+        Red:     carol,
+    })
+    systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+    require.True(t, found)
+    require.EqualValues(t, types.SystemInfo{
+        NextId: 2,
+    }, systemInfo)
+    game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+    require.True(t, found1)
+    require.EqualValues(t, types.StoredGame{
+        Index: "1",
+        Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+        Turn:  "b",
+        Black: bob,
+        Red:   carol,
+    }, game1)
 }
--- x/checkers/keeper/msg_server_test.go
+++ x/checkers/keeper/msg_server_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	keepertest "github.com/arifintahu/checkers/testutil/keeper"
+	"github.com/arifintahu/checkers/x/checkers"
 	"github.com/arifintahu/checkers/x/checkers/keeper"
 	"github.com/arifintahu/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,5 +13,6 @@ import (
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
 }
